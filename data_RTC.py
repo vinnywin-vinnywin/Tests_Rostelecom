c4039bf3398615dd8618ce31d5413470340981b3
vinnywin-vinnywin <143638480+vinnywin-vinnywin@users.noreply.github.com>
2024-05-17 17:59:40 +0300

    Update data_RTC.py

--- data_RTC.py
+++ data_RTC.py
@@ -1,8 +1,8 @@
 
-data_phone_valid = '+7 952 247-19-79'
-data_mail_valid = 'vinnywin@yandex.ru'
-data_login_valid = 'lk_7727308'
-data_licevoi_valid = '278014362998'
+data_phone_valid = '+7 952 247-19-71'
+data_mail_valid = 'vinnywin@yandex1.ru'
+data_login_valid = 'lk_7727301'
+data_licevoi_valid = '278014362991'
 
-data_psw_valid = 'psw_Test'
+data_psw_valid = 'psw_Test1'
 data_psw_INvalid = 'psw_Test_not_correct'
